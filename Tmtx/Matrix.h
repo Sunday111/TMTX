2e2e76307da6324c30a3304d74873661df3f1470
Konstantin Lazukin <konstantin.lazukin@gmail.com>
2017-07-25 09:50:20 +0300

    Signed matrix helpers class

--- Tmtx/Matrix.h
+++ Tmtx/Matrix.h
@@ -18,7 +18,8 @@ namespace matrix_details
 	template<
 		typename T, size_t R, size_t C,
 		template<typename, size_t, size_t> typename Matrix,
-		template<typename, size_t, size_t> typename Final>
+		template<typename, size_t, size_t> typename Final,
+		typename Derived>
 	class BaseMatrixHelper
 	{
 	private:
@@ -55,7 +56,7 @@ namespace matrix_details
 		TFinal& ForEachCell(Args&... args)
 		{
 			ForEachRow<U>(std::make_index_sequence<R>(), args...);
-			return static_cast<TFinal&>(*this);
+			return This();
 		}
 
 		template<typename U, size_t... indices, typename... Args>
@@ -72,59 +73,92 @@ namespace matrix_details
 		{
 			(void)std::initializer_list<int>
 			{
-				(U::Execute<ir, indices>(static_cast<TFinal&>(*this), args...), 0)...
+				(U::Execute<ir, indices>(This(), args...), 0)...
 			};
 		}
+
+		TFinal& This() { return static_cast<TFinal&>(static_cast<Derived&>(*this)); }
+		const TFinal& This() const { return static_cast<const TFinal&>(static_cast<const Derived&>(*this)); }
 	};
 
-	template<
-		typename T, size_t R, size_t C,
-		template<typename, size_t, size_t> typename Matrix,
-		template<typename, size_t, size_t> typename Final>
-	class CommonMatrixHelpers :
-		public BaseMatrixHelper<T, R, C, Matrix, Final>
+	namespace CommonMatrixHelpers
 	{
-		using TFinal = Final<T, R, C>;
-
-	public:
-		TFinal& operator*=(T value)
+		template<
+			typename T, size_t R, size_t C,
+			template<typename, size_t, size_t> typename Matrix,
+			template<typename, size_t, size_t> typename Final>
+		class CommonMatrixHelpers :
+			public BaseMatrixHelper<T, R, C, Matrix, Final, CommonMatrixHelpers<T, R, C, Matrix, Final>>
 		{
-			return ForEachCell<Multiply>(value);
-		}
+			using TFinal = Final<T, R, C>;
 
-		TFinal operator*(T value) const
-		{
-			auto result = static_cast<const TFinal&>(*this);
-			result *= value;
-			return result;
-		}
+		public:
+			TFinal& operator*=(T value)
+			{
+				return ForEachCell<Multiply>(value);
+			}
 
-		TFinal& operator+=(T value)
-		{
-			return ForEachCell<Plus>(value);
-		}
+			TFinal operator*(T value) const
+			{
+				auto result = static_cast<const TFinal&>(*this);
+				result *= value;
+				return result;
+			}
 
-		TFinal operator+(T value) const
-		{
-			auto result = static_cast<const TFinal&>(*this);
-			result += value;
-			return result;
-		}
+			TFinal& operator+=(T value)
+			{
+				return ForEachCell<Plus>(value);
+			}
 
-		template<template<typename, size_t, size_t> typename U>
-		TFinal& operator+=(const U<T, R, C>& value)
-		{
-			return ForEachCell<Plus>(value);
-		}
+			TFinal operator+(T value) const
+			{
+				auto result = static_cast<const TFinal&>(*this);
+				result += value;
+				return result;
+			}
 
-		template<template<typename, size_t, size_t> typename U>
-		TFinal operator+(const U<T, R, C>& value) const
+			template<template<typename, size_t, size_t> typename U>
+			TFinal& operator+=(const U<T, R, C>& value)
+			{
+				return ForEachCell<Plus>(value);
+			}
+
+			template<template<typename, size_t, size_t> typename U>
+			TFinal operator+(const U<T, R, C>& value) const
+			{
+				auto result = static_cast<const TFinal&>(*this);
+				result += value;
+				return result;
+			}
+		};
+	}
+
+	namespace SignedMatrixHelpers
+	{
+		template<
+			typename T, size_t R, size_t C,
+			template<typename, size_t, size_t> typename Matrix,
+			template<typename, size_t, size_t> typename Final,
+			typename Enable = void>
+		class SignedMatrixHelpers :
+			public BaseMatrixHelper<T, R, C, Matrix, Final,
+			SignedMatrixHelpers<T, R, C, Matrix, Final>>
 		{
-			auto result = static_cast<const TFinal&>(*this);
-			result += value;
-			return result;
-		}
-	};
+		public:
+		};
+
+		template<
+			typename T, size_t R, size_t C,
+			template<typename, size_t, size_t> typename Matrix,
+			template<typename, size_t, size_t> typename Final>
+		class SignedMatrixHelpers<T, R, C, Matrix, Final,
+			std::enable_if_t<std::is_signed<T>::value>> :
+			public BaseMatrixHelper<T, R, C, Matrix, Final,
+			SignedMatrixHelpers<T, R, C, Matrix, Final>>
+		{
+		public:
+		};
+	}
 	
 	// 1. Combines mix-ins into one class.
 	// 2. Applies 'empty base class' optimization.
@@ -152,6 +186,20 @@ namespace matrix_details
 	{
 		template<typename T, size_t N>
 		using StaticArray = std::array<T, N>;
+
+		template<typename T, size_t R, size_t C>
+		union UnitedData
+		{
+			StaticArray<StaticArray<T, C>, R> rows;
+		};
+
+		template<typename T>
+		union UnitedData<T, 1, 2>
+		{
+			struct XYZ { T x, y; } xyz;
+			struct IJK { T i, j; } ijk;
+			StaticArray<StaticArray<T, 2>, 1> rows;
+		};
 		
 		template<
 			typename T, size_t R, size_t C,
@@ -164,67 +212,39 @@ namespace matrix_details
 
 			Row& GetRow(size_t index)
 			{
-				return rows[index];
+				return data.rows[index];
 			}
 
 			decltype(auto) GetColumn()
 			{
-				return StaticArrayView<T, C, sizeof(T) * R>(rows.data()->data());
+				return StaticArrayView<T, C, sizeof(T) * R>(data.rows.data()->data());
 			}
 
 			const Row& GetRow(size_t index) const
 			{
-				return rows[index];
+				return data.rows[index];
 			}
 
 			decltype(auto) GetColumn() const
 			{
-				return StaticArrayView<const T, C, sizeof(T) * R>(rows.data()->data());
+				return StaticArrayView<const T, C, sizeof(T) * R>(data.rows.data()->data());
 			}
 
 			template<size_t N>
-			Row& GetRow()
-			{
-				return rows[N];
-			}
+			Row& GetRow() { return data.rows[N]; }
 
 			template<size_t N>
-			const Row& GetRow() const
-			{
-				return rows[N];
-			}
+			const Row& GetRow() const { return data.rows[N]; }
 			
-			StaticArray<Row, R> rows;
-		};
-
-		template<
-			typename T,
-			template<typename, size_t, size_t> typename Matrix,
-			template<typename, size_t, size_t> typename Final>
-		class MatrixData<T, 1, 2, Matrix, Final>
-		{
-		public:
-			using Row = StaticArray<T, 2>;
-
-			Row& GetRow(size_t index)
-			{
-				assert(index > 0);
-				return rows[0];
-			}
-
-			union
-			{
-				struct { T x, y; };
-				struct { T i, j; };
-				StaticArray<Row, 1> rows;
-			};
+			UnitedData<T, R, C> data;
 		};
 	}
 
 	template<typename T, size_t R, size_t C>
 	class Matrix :
 		public MatrixFunctions<T, R, C, Matrix, Matrix,
-			CommonMatrixHelpers>,
+			CommonMatrixHelpers::CommonMatrixHelpers,
+			SignedMatrixHelpers::SignedMatrixHelpers>,
 		public matrix_data_details::MatrixData<T, R, C, Matrix, Matrix>
 	{
 	};
